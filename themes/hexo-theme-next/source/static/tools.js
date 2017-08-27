bbfb68c9b34921e17ee6732a0faab68d2b6a50d4
soone <soone@anyitou.com>
2017-08-27 22:27:10 +0800

    add music
    Signed-off-by: soone <soone@anyitou.com>

--- themes/hexo-theme-next/source/static/tools.js
+++ themes/hexo-theme-next/source/static/tools.js
@@ -8,27 +8,38 @@ function getCookie(a) {
 var exp = new Date();
 exp.setTime(exp.getTime()+1800*1000);
 var curPlayTime = getCookie("curPlayTime");curPlayTime=(curPlayTime==""||curPlayTime==null)?0.1:curPlayTime;
-var audio=document.getElementById("music_curtime"); audio.volume="0.01";
+var audio=document.getElementById("music_curtime"); audio.volume=0.1;
 document.addEventListener('touchstart', function(){
-   audio.play();
+	audio.play();
+	if(audio.currentTime<curPlayTime){
+		audio.currentTime = curPlayTime;
+	}
 }, false);
 $(document).one('WeixinJSBridgeReady', function () {
-    audio.play();
-    audio.setAttribute('src', '/static/music.mp3');
     audio.load();
+    audio.setAttribute('src', '/static/music.mp3');
+	audio.play();
+	if(audio.currentTime<curPlayTime){
+		audio.currentTime = curPlayTime;
+	}
 });
 window.addEventListener('devicemotion', function () {
-    audio.play();
+	audio.play();
+	if(audio.currentTime<curPlayTime){
+		audio.currentTime = curPlayTime;
+	}
 }, false);
 function showLogin() {
 	document.cookie = "curPlayTime=" + escape(audio.currentTime) + ";expires=" + exp.toGMTString() + ";path=/";
-	if(audio.volume<0.5){ audio.currentTime = curPlayTime; }
+	if(audio.volume<0.5){
+		audio.currentTime = curPlayTime; 
+	}
 	if(audio.currentTime>=curPlayTime){ audio.volume=0.98; }
 	if(audio.paused){
 		audio.play();
 	}
 }
 $(document).ready(function(){
-    setInterval("showLogin()","1000");
+    setInterval("showLogin()","800");
 });
 

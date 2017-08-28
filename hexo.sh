78ca50e6bb89e5f0f8d41d965d907748e54ba15f
wangyanchong <wangyanchong@rtmap.com>
2017-08-28 19:11:03 +0800

    change
    Signed-off-by: wangyanchong <wangyanchong@rtmap.com>

--- hexo.sh
+++ hexo.sh
@@ -2,9 +2,8 @@
 ### BEGIN INIT INFO
 #  shell run hexo
 ### END INIT INFO
-cd $1
 #pkill hexo 
 hexo clean
 nohup hexo g -w  > ~/hexo_watch.log 2>&1 &
-nohup hexo d -w  > ~/hexo_watch.log 2>&1 &
+nohup hexo s > ~/hexo_watch.log 2>&1 &
 #nohup hexo s > ~/hexo.log 2>&1 &
